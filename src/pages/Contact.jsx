b32b5fb638fc946044dc4e000e833044671c2e9b
Calvin <creinke@myune.edu.au>
2026-04-26 11:23:28 +1000

    Fix no-useless-escape in email regex; add babel private-property plugin

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -63,7 +63,7 @@ const Contact = () => {
     };
 
     const isValidEmail = (email) => {
-        const emailRegex = /^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         return emailRegex.test(email);
     };
 
