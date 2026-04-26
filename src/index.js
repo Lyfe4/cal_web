17ce408865c4996205d31a1cdfd9139a9c8248d0
Calvin <creinke@myune.edu.au>
2026-04-26 17:32:47 +1000

    crawl help

--- src/index.js
+++ src/index.js
@@ -5,31 +5,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// For improved SEO with react-snap
-const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
 
-if (rootElement.hasChildNodes()) {
-  // Hydrate for react-snap prerendering
-  ReactDOM.hydrateRoot(rootElement, 
-    <React.StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </React.StrictMode>
-  );
-} else {
-  // Normal rendering
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </React.StrictMode>
-  );
-}
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
